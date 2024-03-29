33937f1dc20e8611d9f674faae77cd6e86e26912
Liu Hong Yuan Tom <liuhongyuan3000@gmail.com>
2024-03-29 17:49:02 +0800

    Add docs for mazeGame.h header file

--- Maze/mazeGame.h
+++ Maze/mazeGame.h
@@ -3,6 +3,16 @@
 #include <list>
 using namespace std;
 
+/**
+ * @struct Cell
+ * @brief Represents a cell in the maze.
+ * 
+ * The Cell struct stores the row and column coordinates of a cell in the maze.
+ */
+struct Cell {
+    int row; /**< The row coordinate of the cell. */
+    int col; /**< The column coordinate of the cell. */
+};
 struct Cell {
     int row;
     int col;
