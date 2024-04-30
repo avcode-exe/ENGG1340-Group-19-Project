80cb8955e2f5f5497cd099fca5b2df3bc96578ed
DavidChan115 <159612070+DavidChan115@users.noreply.github.com>
2024-04-30 15:13:10 +0800

    Update main.cpp

--- Maze/main.cpp
+++ Maze/main.cpp
@@ -177,7 +177,7 @@ int main() {
         refresh();
 
         if (msR == 0) {
-          std::this_thread::sleep_for(std::chrono::seconds(2));
+          std::this_thread::sleep_for(std::chrono::seconds(6));
         }
       }
       std::this_thread::sleep_for(std::chrono::milliseconds(500));
